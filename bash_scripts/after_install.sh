cc381db203e18b61596d95a1b2aafd047f8fa655
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 14:53:04 -0400

    afterinstall

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # if anaconda not yet installed, install it
-if [ ! -d "$/var/analytics/anaconda2"]; then
-	bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
-fi
+[[ -d /var/analytics/anaconda2 ]] || bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
 
-apt-get install -y python-pip
 pip install /var/analytics/an_predictions
 pip uninstall -y larkin
 pip install /var/analytics/an_predictions
