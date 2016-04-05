6052f55ef5e95b359b4c66d49bfe931216214db6
Will Jacobson <will@wills-imac.internal.prescriptivedata.io>
2016-04-05 16:36:54 -0400

    y

--- bash_scripts/after_install.sh
+++ bash_scripts/after_install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# if anaconda not yet installed, install it
-[[ -d /var/analytics/anaconda2 ]] || bash /var/analytics/Anaconda2-4.0.0-Linux-x86_64.sh -b -p /var/analytics/anaconda2
-
+export "PATH=/var/analytics/anaconda2/bin:$PATH"
 pip install /var/analytics/an_predictions
 pip uninstall -y larkin
 pip install /var/analytics/an_predictions
